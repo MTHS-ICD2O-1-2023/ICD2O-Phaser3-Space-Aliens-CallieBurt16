419830d05f39273650aa7170ac51be036f0614a3
Callie Burt <159185440+CallieBurt16@users.noreply.github.com>
2024-04-23 18:20:28 +0000

    added code to game.js

--- js/game.js
+++ js/game.js
@@ -1,3 +1,33 @@
   /* global Phaser */
 
-  // Copyright (c) 2020
+  // Copyright (c) 2024 Callie All rights reserved
+  //
+  // Created by: Callie
+  // Created on: Apr 2024
+  // This is the Phaser3 game of configuration file
+
+  /**
+   * Start Phaer Game.
+   */
+  const config = {
+    type: Phaser.AUTO,
+    Width: 1920,
+    height: 1080,
+    pysics: {
+      default: "arcade",
+      arcade: {
+        debug: true,
+      },
+    },
+    // set background color
+    backgroundColor: 0x5f6e7a,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      // we place it in the middle of the page.
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+  }
+
+  const game = new Phaser.Game(config)
+  console.log(game)
+  
